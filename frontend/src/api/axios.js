fc7e981c572ee47ed0d44dda458a7fcd520646ab
martin <meverdiaz@gmail.com>
2025-03-04 21:44:52 -0300

    credendials1.4

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -8,24 +8,3 @@ const client = axios.create({
 });
 
 export default client;
-
-// Función para autenticar al usuario
-const signin = async () => {
-  try {
-    const response = await client.post('/signin', {
-      email: "meverdiaz@gmail.com",  
-      password: "123456"
-    });
-
-    const token = response.data.token; // Recibir el token en la respuesta
-
-    // Guardar el token en el almacenamiento local o en una cookie
-    localStorage.setItem("token", token);
-
-    console.log("Autenticación exitosa:", token);
-  } catch (error) {
-    console.error("Error en la autenticación:", error.response ? error.response.data : error.message);
-  }
-};
-
-signin(); // Llamar a la función para autenticar al usuario
