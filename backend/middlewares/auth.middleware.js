96064dc67e559768829535b11f22016d9102b829
martin <meverdiaz@gmail.com>
2025-03-05 17:59:06 -0300

    solucion-isAuth2

--- backend/middlewares/auth.middleware.js
+++ backend/middlewares/auth.middleware.js
@@ -1,21 +1,21 @@
 import jwt from "jsonwebtoken";
 
 export const isAuth = (req, res, next) => {
-  console.log("Cookies recibidas:", req.cookies); // Depuración
-
-  const token = req.cookies.token; // O usar Authorization si prefieres headers
+  const token = req.cookies.token;
 
   if (!token) {
-    return res.status(401).json({ message: "No estás autorizado, token faltante" });
+    return res.status(401).json({
+      message: "No estas autorizado",
+    });
   }
 
-  jwt.verify(token, "xyz123", (err, decoded) => {
-    if (err) {
-      return res.status(401).json({ message: "Token inválido o expirado" });
-    }
+  jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+    if (err)
+      return res.status(401).json({
+        message: "No estas autorizado",
+      });
 
-    req.userId = decoded.id;
-    console.log("Token decodificado:", decoded);
+    req.userId = decoded.id; // Store the decoded user ID
     next();
   });
 };
